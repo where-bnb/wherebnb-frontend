7fc55ba95a92e7c3f92852fdc6573cfafa08944b
oyeong011 <oyeong011@gmail.com>
2024-01-11 17:14:24 +0900

    Feat: package 변경

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    swcMinify: true,
+    pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+    // app 폴더 내의 pages 디렉토리 사용 설정
+}
 
-module.exports = nextConfig
+module.exports = nextConfig;
