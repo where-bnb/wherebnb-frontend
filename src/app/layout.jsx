8489e0ca9e8b7f724a13f5abd724daea12f25093
oyeong011 <oyeong011@gmail.com>
2024-01-13 03:02:48 +0900

    Feat: 호스트 페이지 navbar컴포넌트

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,11 +1,11 @@
 import { Nunito } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/navbar/Navbar";
 import LoginModal from "../components/ui/modals/LoginModal";
 import RegisterModal from "../components/ui/modals/RegisterModal";
 import ToastProvider from "@/providers/ToastProvider";
 import { MswProvider } from "../providers/MswProvider";
 import AuthSession from "@/providers/AuthSessionProvider";
+import Navbar from "@/components/navbar/Navbar";
 
 export const metadata = {
   title: "WhereBnb",
@@ -16,7 +16,9 @@ const font = Nunito({
   subsets: ["latin"],
 });
 
-export default async function RootLayout({ children }) {
+export default async function RootLayout({ children, navbar }) {
+
+
   return (
     <html lang="en">
       <body className={font.className}>
@@ -25,7 +27,7 @@ export default async function RootLayout({ children }) {
           <ToastProvider />
           <RegisterModal />
           <LoginModal />
-          <Navbar />
+          {navbar || <Navbar/>}
           <div className="md:pt-[200px] pt-[95px]">{children}</div>
         </AuthSession>
       </body>
